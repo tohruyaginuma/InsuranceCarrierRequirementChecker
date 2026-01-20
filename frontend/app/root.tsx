180854ff64c5e5fe9e7ea8dbee4d1d0889a610ee
Tohru Yaginuma <toru45s@gmail.com>
2026-01-20 10:09:51 -0800

    add Toaster

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -7,7 +7,7 @@ import {
   ScrollRestoration,
 } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { Toaster } from "~/components/ui/sonner";
 import type { Route } from "./+types/root";
 import "./app.css";
 
@@ -40,6 +40,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
           <main className="m-auto w-[1280px] p-4">{children}</main>
           <ScrollRestoration />
           <Scripts />
+          <Toaster />
         </QueryClientProvider>
       </body>
     </html>
