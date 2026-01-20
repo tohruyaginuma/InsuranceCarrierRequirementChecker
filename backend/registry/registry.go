995eeb68a7d24bfb228d96954aa3e84e606f0c19
Tohru Yaginuma <toru45s@gmail.com>
2026-01-20 07:58:47 -0800

    update registry

--- backend/registry/registry.go
+++ backend/registry/registry.go
@@ -1,7 +1,11 @@
 package registry
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/tohruyaginuma/InsuranceCarrierRequirementChecker/handler"
+	"github.com/tohruyaginuma/InsuranceCarrierRequirementChecker/model"
 	"github.com/tohruyaginuma/InsuranceCarrierRequirementChecker/store"
 )
 
@@ -9,9 +13,26 @@ type Registry struct {
 	ApplicantHandler applicantHandler
 }
 
+func unmarshalData() model.Data {
+	file, err := os.ReadFile("data/data.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var data model.Data
+	err = json.Unmarshal(file, &data)
+	if err != nil {
+		panic(err)
+	}
+
+	return data
+}
+
 func NewRegistry() *Registry {
+	jsonData := unmarshalData()
+
 	applicantStore := store.NewApplicant()
-	applicantHandler := handler.NewApplicant(applicantStore)
+	applicantHandler := handler.NewApplicant(applicantStore, jsonData.CarrierRequirements)
 
 	return &Registry{
 		ApplicantHandler: applicantHandler,
