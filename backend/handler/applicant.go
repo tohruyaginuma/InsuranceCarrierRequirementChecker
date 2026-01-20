2781b61120d68e90fe867b5e34c8f3e58bd9492b
Tohru Yaginuma <toru45s@gmail.com>
2026-01-20 07:32:53 -0800

    update handler

--- backend/handler/applicant.go
+++ backend/handler/applicant.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"log/slog"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/tohruyaginuma/InsuranceCarrierRequirementChecker/model"
 )
 
 type applicant struct {
@@ -17,12 +19,55 @@ func NewApplicant(store applicantStore) *applicant {
 }
 
 func (h *applicant) Create(c echo.Context) error {
-	return c.JSON(http.StatusOK, map[string]any{
-		"result": "OK",
+	slog.Debug("applicant create")
+
+	var request createApplicantRequest
+	if err := c.Bind(&request); err != nil {
+		slog.Debug("applicant create bind failed", "error", err)
+		return c.JSON(http.StatusBadRequest, map[string]any{
+			"result":  "NG",
+			"message": "invalid request body",
+		})
+	}
+
+	applicant := model.Applicant{
+		GivenName:       request.GivenName,
+		Surname:         request.Surname,
+		DateOfBirth:     request.DateOfBirth,
+		InsuranceStatus: request.InsuranceStatus,
+		PriorCarrier:    request.PriorCarrier,
+		UMPD:            request.UMPD,
+		Collision:       request.Collision,
+	}
+
+	applicantID := h.store.Create(c.Request().Context(), applicant)
+
+	return c.JSON(http.StatusCreated, map[string]any{
+		"result":       "OK",
+		"applicant_id": applicantID,
 	})
 }
+
 func (h *applicant) List(c echo.Context) error {
+	slog.Debug("applicant list")
+
+	applicants := h.store.List(c.Request().Context())
+
+	applicantResponse := make([]listApplicantResponse, len(applicants))
+	for i, a := range applicants {
+		applicantResponse[i] = listApplicantResponse{
+			GivenName:       a.GivenName,
+			Surname:         a.Surname,
+			DateOfBirth:     a.DateOfBirth,
+			InsuranceStatus: a.InsuranceStatus,
+			PriorCarrier:    a.PriorCarrier,
+			UMPD:            a.UMPD,
+			Collision:       a.Collision,
+		}
+	}
+
 	return c.JSON(http.StatusOK, map[string]any{
-		"result": "OK",
+		"result":     "OK",
+		"applicants": applicantResponse,
 	})
 }
---
+++ backend/handler/applicant_dto.go
@@ -0,0 +1,21 @@
+package handler
+
+type listApplicantResponse struct {
+	GivenName       string `json:"given_name"`
+	Surname         string `json:"surname"`
+	DateOfBirth     string `json:"date_of_birth"`
+	InsuranceStatus string `json:"insurance_status"`
+	PriorCarrier    string `json:"prior_carrier"`
+	UMPD            *int   `json:"umpd"`
+	Collision       *int   `json:"collision"`
+}
+
+type createApplicantRequest struct {
+	GivenName       string `json:"given_name"`
+	Surname         string `json:"surname"`
+	DateOfBirth     string `json:"date_of_birth"`
+	InsuranceStatus string `json:"insurance_status"`
+	PriorCarrier    string `json:"prior_carrier"`
+	UMPD            *int   `json:"umpd"`
+	Collision       *int   `json:"collision"`
+}
