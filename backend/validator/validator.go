3a34319ac7f3d3bb307c019ac0ed6ba5ef98079b
Tohru Yaginuma <toru45s@gmail.com>
2026-01-20 07:57:26 -0800

    update validator

--- backend/validator/validator.go
+++ backend/validator/validator.go
@@ -37,7 +37,7 @@ func validateRegex(value any, test string) bool {
 	return matched
 }
 
-func CheckValidator(value any, validator model.Validator) bool {
+func checkValidator(value any, validator model.Validator) bool {
 	switch validator.Type {
 	case "equals":
 		return validateEquals(value, validator.Values)
@@ -48,4 +48,81 @@ func CheckValidator(value any, validator model.Validator) bool {
 	}
 
 	return false
+}
+
+func findTargetFieldValue(propertyName string, fieldValues []model.FieldValue) (fieldValue *model.FieldValue) {
+	for _, v := range fieldValues {
+		if v.PropertyName == propertyName {
+			return &v
+		}
+	}
+	return nil
+}
+
+func everyFieldValidationResult(results []model.FieldValidationResult) (result bool) {
+	for _, r := range results {
+		if !r.IsValid {
+			return false
+		}
+	}
+
+	return true
+}
+
+func ValidateCarrierRequirements(fieldValues []model.FieldValue, requirements []model.CarrierRequirement) (results model.ValidationResult) {
+	fieldValidationResult := make([]model.FieldValidationResult, len(requirements))
+
+	for i, r := range requirements {
+		if len(r.Validator.Conditions) > 0 {
+			var results = make([]bool, len(r.Validator.Conditions))
+			for i, c := range r.Validator.Conditions {
+				targetFieldValueForCondition := findTargetFieldValue(c.PropertyName, fieldValues)
+
+				if targetFieldValueForCondition != nil {
+					results[i] = checkValidator(targetFieldValueForCondition.Value, c.Validator)
+				}
+			}
+
+			var result bool
+			for _, r := range results {
+				if !r {
+					result = false
+					break
+				}
+
+				result = true
+			}
+			if !result {
+				fieldValidationResult[i] = model.FieldValidationResult{
+					PropertyName: r.PropertyName,
+					IsValid:      true,
+				}
+				continue
+			}
+		}
+
+		targetFieldValue := findTargetFieldValue(r.PropertyName, fieldValues)
+
+		var isValid bool
+		if targetFieldValue != nil {
+			isValid = checkValidator(targetFieldValue.Value, r.Validator)
+		}
+		var errorMessage string
+		if !isValid {
+			errorMessage = r.Explanation
+		}
+
+		fieldValidationResult[i] = model.FieldValidationResult{
+			PropertyName: r.PropertyName,
+			IsValid:      isValid,
+			ErrorMessage: errorMessage,
+		}
+	}
+
+	isValid := everyFieldValidationResult(fieldValidationResult)
+
+	return model.ValidationResult{
+		IsValid:      isValid,
+		FieldResults: fieldValidationResult,
+	}
 }
