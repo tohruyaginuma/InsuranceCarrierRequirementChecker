a1459fe2e1f25678113f0fa504a984df9f136611
Tohru Yaginuma <toru45s@gmail.com>
2026-01-20 08:47:27 -0800

    add components

---
+++ frontend/app/components/heading.tsx
@@ -0,0 +1,16 @@
+import type { PropsWithChildren } from "react";
+
+type Props = PropsWithChildren<{
+	title: string;
+}>;
+
+const Heading = (props: Props) => {
+	const { title } = props;
+	return (
+		<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">
+			{title}
+		</h1>
+	);
+};
+
+export default Heading;
